28c4e49c0b7d95358eff32d45f7c995be74443ab
mirzaibtisam41 <mirzaibtisam41@gmail.com>
2025-10-23 20:33:27 +0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AppProvider } from './context/AppContext';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {AppProvider} from './context/AppContext';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
@@ -9,20 +9,34 @@ import Checkout from './pages/Checkout';
 import OrderSummary from './pages/OrderSummary';
 
 function App() {
+  const login = () => {
+    fetch('https://cubecrm-backend.onrender.com/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        email: 'muhammadibtisam@vague.so',
+        password: '123456',
+      }),
+    });
+  };
   return (
-    <AppProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/dish/:id" element={<DishDetails />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/order-summary/:orderId" element={<OrderSummary />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </AppProvider>
+    <button onClick={login}>Login</button>
+    // <AppProvider>
+    //   <Router>
+    //     <Layout>
+    //       <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/menu" element={<Menu />} />
+    //         <Route path="/dish/:id" element={<DishDetails />} />
+    //         <Route path="/checkout" element={<Checkout />} />
+    //         <Route path="/order-summary/:orderId" element={<OrderSummary />} />
+    //       </Routes>
+    //     </Layout>
+    //   </Router>
+    // </AppProvider>
   );
 }
 
